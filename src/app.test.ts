65d9d9cc885f51c3fbb22d58c79eccf28a4b4f7d
Stefan Tuertscher <stefan@tsl-tuertscher.com>
2021-04-20 17:45:11 +0200

    1.1.0

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,18 +1,15 @@
-﻿import { Hgt, HgtInterface } from "./app";
+﻿import { Hgt, HgtInterface } from './app';
 
 const config: HgtInterface = {
   tessellation: 180,
   gridSize: 1201,
-  filePath:"./test/1201/"
-}
+  filePath: './test/1201/',
+};
 
 describe('Hgt', () => {
   it('Hgt', async () => {
-      const hgt = new Hgt(config)
-      const elevation = await hgt.getElevationFromCoordinate([
-        9.47,
-        47.34
-      ])
-      expect(Math.round(elevation)).toBe(1078);
+    const hgt = new Hgt(config);
+    const elevation = await hgt.getElevationFromCoordinate([9.47, 47.34]);
+    expect(Math.round(elevation)).toBe(1078);
   });
 });
